1269145a2536a89f5456d1ff661ba81191fc2938
Davide Rubiu <rubiu@dauvea.it>
2024-10-23 12:02:31 +0200

    Fixing mailer

--- app/utils/email.js
+++ app/utils/email.js
@@ -2,20 +2,39 @@ const logger = require('./logger');
 const { startSpinner } = require('./spinner');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
-const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: process.env.MAIL_SECURE === 'true',
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-    },
-});
+const fs = require('fs');
+
+const mapTransporter = () => {
+    let transporterBody = {
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        secure: process.env.MAIL_SECURE === 'true',
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS
+        },
+        tls:{
+            rejectUnAuthorized:true
+        }
+    }
+    if(process.env.ALLOW_DKIM === 'true'){
+        const dkimPrivate = fs.readFileSync(process.env.DKIM_PATH_PRIVATE, 'utf8');
+        transporterBody.dkim = {
+            domainName: process.env.DKIM_DOMAIN,
+            keySelector: process.env.DKIM_SELECTOR,  // lo stesso selettore usato nel DNS
+            privateKey: dkimPrivate
+        }
+    }
+
+
+    return transporterBody
+}
+const transporter = nodemailer.createTransport(mapTransporter());
 const sendEmailTransporter = async (infos) => {
     try {
-        const intervalId = startSpinner();
+        //const intervalId = startSpinner();
         await transporter.sendMail(infos);
-        clearInterval(intervalId);
+        //clearInterval(intervalId);
         console.log('\n');
         logger.info('Email sent to: ' + infos.to);
     } catch (error) {
